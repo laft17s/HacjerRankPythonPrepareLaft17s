f3715423df9e645feb74d894739a98efbe96037f
Sean17nl <sean17nl@gmail.com>
2021-12-18 12:31:42 -0500

    print & operatos

---
+++ helloWorld.py
@@ -0,0 +1,13 @@
+# Print: "Hello, World!"
+
+# >> SAMPLE OUTPUT
+#   Hello, World!
+
+if __name__ == '__main__':
+
+    # SOLUTION 1
+    print("Hello, World!")
+
+    # SOLUTION 2
+    my_string = "Hello, Luis!"
+    print(my_string)
